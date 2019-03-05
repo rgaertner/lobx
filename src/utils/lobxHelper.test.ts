dbe53d7f0cfe956234e02768a4748d977c71363c
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-03-05 23:55:13 +0100

    feat: add helpers to check and extract lobx ids

---
+++ src/utils/lobxHelper.test.ts
@@ -0,0 +1,77 @@
+import { computed, action } from '../lobx';
+import {
+  hasLobxId,
+  getLobxId,
+  getInstanceLobxId,
+  hasWrappedLobxId,
+  hasInstanceLobxId,
+  getWrappedLobxId,
+  hasGetterLobxId,
+  getGetterLobxId,
+  hasSetterLobxId,
+  getSetterLobxId
+} from './lobxHelper';
+
+class TestAction {
+  public fn = jest.fn();
+
+  @computed
+  public get testCallToGetter() {
+    return 2;
+  }
+
+  @computed
+  public get wurst(): number {
+    this.fn(this, 'getter', 5);
+    return 5;
+  }
+
+  public set wurst(a1: number) {
+    this.fn(this, 'setter', a1);
+  }
+
+  @action
+  public call() {
+    return 'called';
+  }
+}
+test('get LobxRef from class instance', () => {
+  const test = new TestAction();
+  test.call();
+  expect(hasLobxId(test)).toBe(true);
+  expect(hasInstanceLobxId(test)).toBe(true);
+  expect(getInstanceLobxId(test).length).toBeGreaterThan(20);
+  expect(getLobxId(test).length).toBeGreaterThan(20);
+});
+
+test('get LobxRef from callback instance', () => {
+  const test = new TestAction();
+  test.call();
+  expect(hasWrappedLobxId(test.call)).toBe(true);
+  expect(hasLobxId(test.call)).toBe(true);
+  expect(getWrappedLobxId(test.call).length).toBeGreaterThan(20);
+  expect(getLobxId(test.call).length).toBeGreaterThan(20);
+});
+
+test('get LobxRef from get field', () => {
+  const test = new TestAction();
+  expect(test.wurst).toBe(5);
+  (test as any).getterName = 'wurst';
+  expect(hasLobxId(test)).toBe(true);
+  expect(hasGetterLobxId(test)).toBe(true);
+  expect(getGetterLobxId(test).length).toBeGreaterThan(20);
+  expect(getLobxId(test).length).toBeGreaterThan(20);
+});
+
+test('get LobxRef from set field', () => {
+  const test = new TestAction();
+  test.wurst = 6;
+  (test as any).setterName = 'wurst';
+  expect(hasLobxId(test)).toBe(true);
+  expect(hasSetterLobxId(test)).toBe(true);
+  const setterId = getSetterLobxId(test);
+  expect(setterId.length).toBeGreaterThan(20);
+  const instanceId = getInstanceLobxId(test);
+  expect(instanceId.length).toBeGreaterThan(20);
+  expect(setterId === instanceId).toBe(false);
+});
---
+++ src/utils/lobxHelper.ts
@@ -0,0 +1,131 @@
+import { LobxRef } from '../action-chain/action-chain';
+
+// does that really make sense? Mixing look up for instance and mehod ids
+export function hasLobxId(instance: any): boolean {
+  const checkFunctions: ((i: any) => boolean)[] = [
+    hasInstanceLobxId,
+    hasWrappedLobxId,
+    hasGetterLobxId,
+    hasSetterLobxId
+  ];
+  const id = checkFunctions.reduce((hasId, fun) => {
+    if (!hasId) {
+      hasId = fun(instance);
+    }
+    return hasId;
+  }, false);
+  return id;
+}
+
+export function hasInstanceLobxId(instance: any): boolean {
+  debugger;
+  if (instance.hasOwnProperty('__lobx')) {
+    return (instance as LobxRef).__lobx.hasOwnProperty('id');
+  }
+  return false;
+}
+
+export function hasDecoratedLobxId(instance: any): boolean {
+  if (instance.hasOwnProperty('methodName')) {
+    return hasGetSetLobxId(instance, instance.methodName, 'value');
+  }
+  return false;
+}
+
+export function hasWrappedLobxId(instance: any) {
+  if (instance.hasOwnProperty('__lobx')) {
+    if (instance.__lobx.hasOwnProperty('wrapped')) {
+      return hasInstanceLobxId(instance.__lobx.wrapped);
+    }
+  }
+  return false;
+}
+export function hasGetterLobxId(instance: any): boolean {
+  if (instance.hasOwnProperty('getterName')) {
+    return hasGetSetLobxId(instance, instance.getterName, 'get');
+  }
+  return false;
+}
+
+function hasGetSetLobxId(
+  instance: any,
+  name: string,
+  method: 'get' | 'set' | 'value'
+) {
+  const operation = Object.getOwnPropertyDescriptor(
+    Object.getPrototypeOf(instance),
+    name
+  )[method];
+  if (operation) {
+    return hasWrappedLobxId(operation);
+  }
+  return false;
+}
+
+function getGetSetLobxId(
+  instance: any,
+  name: string,
+  method: 'get' | 'set' | 'value'
+) {
+  if (hasGetSetLobxId(instance, name, method)) {
+    const operation = Object.getOwnPropertyDescriptor(
+      Object.getPrototypeOf(instance),
+      name
+    )[method];
+    return getWrappedLobxId(operation);
+  }
+  return '';
+}
+
+export function hasSetterLobxId(instance: any): boolean {
+  if (instance.hasOwnProperty('setterName')) {
+    return hasGetSetLobxId(instance, instance.setterName, 'set');
+  }
+  return false;
+}
+
+// does that really make sense? Mixing look up for instance and mehod ids
+export function getLobxId(instance: any) {
+  debugger;
+  const idFunctions: ((i: any) => string)[] = [
+    getInstanceLobxId,
+    getWrappedLobxId,
+    getGetterLobxId,
+    getSetterLobxId
+  ];
+  const id = idFunctions.reduce((id, fun) => {
+    if (id.length === 0) {
+      id = fun(instance);
+    }
+    return id;
+  }, '');
+  return id;
+}
+
+export function getInstanceLobxId(instance: any): string {
+  if (hasInstanceLobxId(instance)) {
+    return (instance as LobxRef).__lobx.id;
+  }
+  return '';
+}
+
+export function getWrappedLobxId(instance: any): string {
+  if (hasWrappedLobxId(instance)) {
+    return (instance.__lobx.wrapped as LobxRef).__lobx.id;
+  }
+  return '';
+}
+
+export function getGetterLobxId(instance: any): string {
+  debugger;
+  if (hasGetterLobxId(instance)) {
+    return getGetSetLobxId(instance, instance.getterName, 'get');
+  }
+  return '';
+}
+export function getSetterLobxId(instance: any): string {
+  if (hasSetterLobxId(instance)) {
+    return getGetSetLobxId(instance, instance.setterName, 'set');
+  }
+  return '';
+}
